9c7fd15290af1c0c154874e736c0c48d09ea15b2
andrea <marani.andrea@gmail.com>
2026-05-24 18:05:46 +0200

    Removed unnecessary files

--- test.py
+++
@@ -1,3 +0,0 @@
-import subprocess
-#subprocess.run(["mpv", "--no-video", "https://stream.motherearthradio.de/listen/motherearth_klassik/motherearth.klassik.flac-lo"])
-subprocess.run(["mpv", "--no-video", "https://stream.motherearthradio.de/listen/motherearth_jazz/motherearth.jazz.mpq"])
